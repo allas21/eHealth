9964ce055ff634a7e37f2a3ae03c5a51fdb93225
katerinapat <katerinaki_pat07@hotmail.gr>
2014-06-04 21:26:53 +0300

    first commit

---
+++ ehealth/footer.php
@@ -0,0 +1,21 @@
+<div class="clear"> </div>
+		   <div class="footer">
+		   	 <div class="wrap">
+		   	<div class="footer-left">
+		   			<ul>
+						<li><a href="index.html">Home</a></li>
+						<li><a href="about.html">About</a></li>
+						<li><a href="services.html">services</a></li>
+						<li><a href="blog.html">blog</a></li>
+						<li><a href="contact.html">contact</a></li>
+					</ul>
+		   	</div>
+		   	<div class="footer-right">
+		   		<p>Medicare | Design By <a href="http://w3layouts.com/">W3Layouts</a></p>
+		   	</div>
+		   	<div class="clear"> </div>
+		   </div>
+		   </div>
+		<!--end-wrap-->
+	</body>
+</html>
